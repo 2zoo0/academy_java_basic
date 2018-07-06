13f380782e8052f949a26defd1e7aac07fd301d0
2zoo0 <asqw2323@naver.com>
2018-07-06 11:50:57 +0900

    java foreach

--- day02-ControlFlow/src/DevilNineNine.java
+++ day02-ControlFlow/src/DevilNineNine.java
@@ -1,9 +1,21 @@
+import java.util.Random;
 
 public class DevilNineNine {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Random random = new Random();
+		int randomInt01 = random.nextInt(8);
+		String[] lmenu; 
+		lmenu = new String[10];
+		lmenu[0] = "맛존";
+		lmenu[1] = "고씨네";
+		lmenu[2] = "도스마스";
+		lmenu[3] = "사이공 쌀국수";
+		lmenu[4] = "맘스터치";
+		lmenu[5] = "백반";
+		lmenu[6] = "뚝배기";
+		lmenu[7] = "행컵";
+		System.out.printf("오늘의 점심은 '%s' ", lmenu[randomInt01]);
 	}
 
 }
